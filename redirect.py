3e8706824c194ca908ec87dc6529c09eb195aaec
hackers-terabit <terabit.funtoo@yandex.com>
2016-03-29 07:22:18 +0000

    redirect.py updated

--- redirect.py
+++ redirect.py
@@ -8,7 +8,8 @@ def request(context, flow):
 
     if re.match("http:\/\/build\.funtoo\.org\/distfiles\/sysresccd\/systemrescuecd-x86-.*\.iso", flow.request.url) is not None:
         flow.request.url = "http://172.16.10.250/systemrescuecd-x86-4.7.1.iso"
-
+        flow.request.host = "172.16.10.250"
     if re.match("http:\/\/build\.funtoo\.org\/funtoo-current\/x86-64bit\/generic_64\/stage3.*", flow.request.url) is not None:
         flow.request.url = "http://172.16.10.250/stage3-latest.tar.xz"
-
+        flow.request.host = "172.16.10.250"
+        
